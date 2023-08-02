905876a101e81c407bb1caa62d6c171d05c097e1
Phoenix-Greenleaf <109920548+Phoenix-Greenleaf@users.noreply.github.com>
2023-08-01 23:59:12 -0700

    Data Structure (probably) operational.
    The Year Task Data file with Task Data are initializing properly, we will see about accessing them next. Kind of a lot of back tracking, tried construct arrays with code but got nowhere. Good learning and practice, but I hope to see some kind of data structure node/handler soon in Godot.

--- data/resources/task_data.gd
+++ data/resources/task_data.gd
@@ -3,14 +3,14 @@ class_name TaskData
 
 @export var task_id: int
 @export var task: String
-@export var section: Section
-@export var month: Month
+@export var section: DataGlobal.Section
+@export var month: DataGlobal.Month
 @export var year: int
 @export var group: String
 @export var task_description: String
 @export var responsible_parties: String
-@export var time_of_day: TimeOfDay
-@export var priority: Priority
+@export var time_of_day: DataGlobal.TimeOfDay
+@export var priority: DataGlobal.Priority
 @export var location: String
 @export var cycle: Array
 @export var days_when_skipped: int
@@ -18,58 +18,13 @@ class_name TaskData
 
 var checkbox_data: Array
 
-var section_keys: Array = Section.keys()
-var month_keys: Array = Month.keys()
+var section_keys: Array = DataGlobal.Section.keys()
+var month_keys: Array = DataGlobal.Month.keys()
 
-enum Checkbox {
-	NOT_ASSIGNED,
-	ASSIGNED,
-	IN_PROGRESS,
-	COMPLETED,
-	EXPIRED,
-}
 
-enum Section {
-	YEARLY,
-	MONTHLY,
-	WEEKLY,
-	DAILY,
-}
 
-enum Month {
-	ALL,
-	JANUARY,
-	FEBRUARY,
-	MARCH,
-	APRIL,
-	MAY,
-	JUNE,
-	JULY,
-	AUGUST,
-	SEPTEMBER,
-	OCTOBER,
-	NOVEMBER,
-	DECEMBER,
-}
-
-enum TimeOfDay {
-	ANY,
-	FIRST_THING,
-	MORNING,
-	AFTERNOON,
-	EVENING,
-	LAST_THING,
-}
-
-enum Priority {
-	NO_PRIORITY,
-	LOW_PRIORITY,
-	NORMAL_PRIORITY,
-	HIGH_PRIORITY,
-	MAX_PRIORITY_OVERRIDE,
-}
-
-func _init(year_init: int, section_init: Section, _month_init: Month = Month.ALL) -> void:
+func _init(year_init: int, section_init: DataGlobal.Section,
+		_month_init: DataGlobal.Month = DataGlobal.Month.ALL) -> void:
 	year = year_init
 	section = section_init
 	month = _month_init
@@ -78,18 +33,46 @@ func _init(year_init: int, section_init: Section, _month_init: Month = Month.ALL
 	var month_label: String = month_keys[month]
 	section_label = section_label.capitalize()
 	month_label = month_label.capitalize()
-	print( "Task Init: ", year, " ", month_label, " ", section_label)
+	print( "TaskData initializing: ", year, " ", month_label, " ", section_label)
 	
 	match section:
-		Section.YEARLY:
-			print("Yearly")
-		Section.MONTHLY:
-			print("Monthly")
-		Section.WEEKLY:
-			print("Weekly")
-		Section.DAILY:
-			print("Daily")
+		DataGlobal.Section.YEARLY:
+			create_checkbox_data()
+			print_complete(section_label)
+		DataGlobal.Section.MONTHLY:
+			create_checkbox_data(12)
+			print_complete(section_label)
+		DataGlobal.Section.WEEKLY:
+			create_checkbox_data(5)
+			print_complete(section_label)
+		DataGlobal.Section.DAILY:
+			match month:
+				DataGlobal.Month.APRIL, DataGlobal.Month.JUNE, \
+				DataGlobal.Month.SEPTEMBER, DataGlobal.Month.NOVEMBER:
+					create_checkbox_data(30)
+				DataGlobal.Month.FEBRUARY:
+					if (year % 4) == 0:
+						create_checkbox_data(29)
+					else:
+						create_checkbox_data(28)
+				DataGlobal.Month.JANUARY, DataGlobal.Month.MARCH, DataGlobal.Month.MAY, DataGlobal.Month.JULY, DataGlobal.Month.AUGUST, DataGlobal.Month.OCTOBER, DataGlobal.Month.DECEMBER:
+					create_checkbox_data(31)
+				_:
+					create_checkbox_data(69)
+			print_complete(section_label)
 		_:
-			print("Some kind of oops")
-	
-	
+			print("Some kind of oops: ", section_label)
+
+
+
+
+func print_complete(section_label: String) -> void:
+	var completext = " section completed"
+	print(section_label, completext)
+	print()
+
+
+func create_checkbox_data(checkbox_length: int = 1) -> void:
+	checkbox_data.resize(checkbox_length)
+	checkbox_data.fill(DataGlobal.Checkbox.ASSIGNED)
+	print(checkbox_length, " checkbox(es) created")
