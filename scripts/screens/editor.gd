fb0a2fc1645936f32ed4d2ebb2f5d23f4efb856e
Phoenix-Greenleaf <109920548+Phoenix-Greenleaf@users.noreply.github.com>
2023-08-05 19:05:23 -0700

    Editor sections prototypes
    The sidebar UI is starting to look real. Discovered button groups with toggle buttons.

--- scripts/screens/editor.gd
+++ scripts/screens/editor.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var menu_button:= $MainMargin/MainHBox/SideMenuVBox/MenuButton as MenuButton
 @onready var current_date_label:= $MainMargin/MainHBox/NonSideVBox/TopMenuHBox/CurrentDateLabel as Label
-
+@onready var current_section_button = 	$MainMargin/MainHBox/SideMenuVBox/YearlyButton
 
 var Weekday : Array = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 var Month : Array = ["Month Index", "January", "February", "March", "April", "May", "June", "July",
@@ -10,9 +10,13 @@ var Month : Array = ["Month Index", "January", "February", "March", "April", "Ma
 
 
 func _ready() -> void:
-	var popup:= menu_button.get_popup()
-	popup.connect("id_pressed", menu_button_actions)
+	connect_menu_button_popup()
+	set_current_date_label()
+	print_ready()
 	
+
+
+func set_current_date_label():
 	var current_date: Dictionary = Time.get_date_dict_from_system()
 	var current_weekday: String =  Weekday[current_date["weekday"]] + " "
 	var current_day: String = str(current_date["day"]) + ", "
@@ -20,8 +24,13 @@ func _ready() -> void:
 	var current_year: String = str(current_date["year"])
 	var current_label:= "Today: " + current_weekday + current_day + current_month + current_year
 	current_date_label.set_text(current_label)
-	
 
+func connect_menu_button_popup():
+	var popup:= menu_button.get_popup()
+	popup.connect("id_pressed", menu_button_actions)
+
+func print_ready():
+	print("Yearly Editor Scene Ready!")
 
 func menu_button_actions(id: int):
 	match id:
@@ -31,6 +40,8 @@ func menu_button_actions(id: int):
 			print("Save File Pressed")
 		2:
 			print("Change Logs Pressed")
+		3:
+			get_tree().quit()
 
 
 
