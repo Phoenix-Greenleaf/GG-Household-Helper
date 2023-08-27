3320709673ab285fc99a6738483fe011d5035c60
Phoenix-Greenleaf <109920548+Phoenix-Greenleaf@users.noreply.github.com>
2023-08-27 15:37:50 -0700

    Task Spreadsheet Data connected and tested
    The gridcontainer now runs an initialization test if no data is loaded into it. GridContainer makes the TaskSpreadsheetData which in turn makes the TaskData(and their checkboxes). Spreadsheet data also calls the taskdata print. Different defaults at different stages tell me if the data is loading properly. Also learned the importance of the separate parameter variable in the init.

--- gui/task_spreadsheet/task_spreadsheet_data.gd
+++ gui/task_spreadsheet/task_spreadsheet_data.gd
@@ -2,4 +2,28 @@ extends Resource
 
 class_name TaskSpreadsheetData
 
+@export var task_year : int
 @export var task_data: Array[TaskData]
+
+
+func _init(task_year_parameter: int = 1990) -> void:
+	task_year = task_year_parameter
+	prints("Spreadsheet Date:", task_year)
+
+
+func initialization_test():
+	var test_task = TaskData.new(
+		"Sheet Test Default",
+		DataGlobal.Section.WEEKLY,
+		"Sheet Test Group",
+		DataGlobal.user_profiles[0],
+		DataGlobal.TimeOfDay.AFTERNOON,
+		DataGlobal.Priority.HIGH_PRIORITY,
+		"Behind You",
+		"week",
+		2,
+		1,
+		"1990-12-25",
+		task_year,
+		)
+	test_task.print_task_data()
--- gui/task_spreadsheet/task_spreadsheet_grid_container.gd
+++ gui/task_spreadsheet/task_spreadsheet_grid_container.gd
@@ -1,8 +1,17 @@
 extends GridContainer
 
-
+@export var data_for_spreadsheet : TaskSpreadsheetData
 
 func _ready() -> void:
-	var test2 = TaskData.new()
-	test2.print_task_data()
+	if !data_for_spreadsheet:
+		test_spreadsheet_initialization()
+	else:
+		print("Spreadsheet data found!")
+	
 
+func test_spreadsheet_initialization() -> void:
+	print("No data found for spreadsheet, running test instead.")
+	var test_year = 1992
+	prints("Testing Year", test_year)
+	var test = TaskSpreadsheetData.new(test_year)
+	test.initialization_test()
