957d6495367cc5d438c4c57abdb693d62d44befa
Phoenix-Greenleaf <109920548+Phoenix-Greenleaf@users.noreply.github.com>
2024-04-05 01:03:56 -0700

    Created column header prototype.
    Created the scene and started the script for a column header. The header options are for sorting.

---
+++ gui/task_tracking/task_spreadsheet/column_header.gd
@@ -0,0 +1,7 @@
+extends PanelContainer
+
+
+@onready var options_panel_container: PanelContainer = %OptionsPanelContainer
+@onready var sorting_button: Button = %SortingButton
+@onready var order_spin_box: SpinBox = %OrderSpinBox
+@onready var header_label: Label = %HeaderLabel
