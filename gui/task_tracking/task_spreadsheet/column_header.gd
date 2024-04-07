767e58fdbd4b1ca9fe892ce80e2ccb80e5ae3896
Phoenix-Greenleaf <109920548+Phoenix-Greenleaf@users.noreply.github.com>
2024-04-06 22:38:31 -0700

    Header prep and initial code
    -Added a grid container in a scroll container for the header row.
    -Initial code for header and toggling visibility.
    -Header label was replaced by a flat button.

--- gui/task_tracking/task_spreadsheet/column_header.gd
+++ gui/task_tracking/task_spreadsheet/column_header.gd
@@ -4,4 +4,24 @@ extends PanelContainer
 @onready var options_panel_container: PanelContainer = %OptionsPanelContainer
 @onready var sorting_button: Button = %SortingButton
 @onready var order_spin_box: SpinBox = %OrderSpinBox
-@onready var header_label: Label = %HeaderLabel
+@onready var header_button: Button = %HeaderButton
+
+
+var sorting_modes = [
+	"None",
+	"Ascend",
+	"Descend",
+]
+
+
+
+func _ready() -> void:
+	header_options_visible(false)
+
+
+func header_options_visible(visible_parameter: bool) -> void:
+	options_panel_container.visible = visible_parameter
+
+
+func _on_header_button_toggled(toggled_on: bool) -> void:
+	header_options_visible(toggled_on)
